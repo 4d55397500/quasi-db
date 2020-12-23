fce08dca8b70c44f9d1cde9057f357a8b10f6eb9
redwrasse <redwrasse@users.noreply.github.com>
2020-12-22 19:55:21 -0700

    add more entries in sample run

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "tnode.h"
 #include "db.h"
 #include "index.h"
@@ -8,19 +9,32 @@
 
 int main() {
 
+    srand(time(NULL));
+
     struct db *database = NULL;
-    printf("Writing to database ...\n");
-    database = add(database, "foo");
-    add(database, "bar");
-    add(database, "who");
+
+    int n = 6;
+    char s[n];
+    char *key;
+    int m = 10000;
+    int k = rand() % m;
+
+    printf("Writing to database %d entries ...\n", m);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            s[j] = '0' + rand() % 72;
+        }
+        if (i == k) {
+            key = s;
+        }
+        database = add(database, s);
+    }
 
     printf("Building index ...\n");
     struct tnode *index = NULL;
     index = buildindex(database);
 
     struct db *rowptr;
-
-    char *key = "bar";
     printf("Looking up key '%s' in index ...\n", key);
     rowptr = lookup(index, key);
     printf("Retrieved pointer %p from index "
